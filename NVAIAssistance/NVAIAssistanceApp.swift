e4b81129f0744db7a774f673f7e4f2076abacc07
namanAstapartners <namanmvaishnav@gmail.com>
2023-12-03 11:34:35 +0530

    Initial Commit

---
+++ NVAIAssistance/NVAIAssistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVAIAssistanceApp.swift
+//  NVAIAssistance
+//
+//  Created by Naman on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NVAIAssistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
