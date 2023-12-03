e9f0107fadd026c6defec936790e3767ae128a01
namanAstapartners <namanmvaishnav@gmail.com>
2023-12-03 12:47:42 +0530

    add view design

---
+++ NVAIAssistance/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  NVAIAssistance
+//
+//  Created by Naman on 03/12/23.
+//
+
+import Foundation
---
+++ NVAIAssistance/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  NVAIAssistance
+//
+//  Created by Naman on 03/12/23.
+//
+
+import Foundation
